51746a130407603bf2f57fe17eec585801333d8e
w.kuczabski <volodymyrkuchabskyy@gmail.com>
2022-06-23 18:53:30 +0200

    [init] - project skeleton created

---
+++ src/main/java/pl/sapiens/javarestsoap/HelloApplication.java
@@ -0,0 +1,9 @@
+package pl.sapiens.javarestsoap;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
